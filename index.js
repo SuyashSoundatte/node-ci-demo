734f0b9e7a6755348eeb965b7e44d19c8f534676
SuyashSoundatte <suyashsoundatte7@gmail.com>
2026-02-25 23:28:51 +0530

    test1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD is working 🚀");
+  res.send("Learning how CI/CD is working 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
